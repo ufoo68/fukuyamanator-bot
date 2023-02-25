75e75dedcbe55e6ac32d828297641b09dcebdeb3
ufoo68 <mitarou.yuutarou@gmail.com]>
2023-02-25 16:22:30 +0900

    message type の分岐

--- src/functions/bot/handler.ts
+++ src/functions/bot/handler.ts
@@ -3,6 +3,7 @@ import * as Line from '@line/bot-sdk'
 import * as Types from '@line/bot-sdk/lib/types'
 import { middyfy } from '@libs/lambda'
 import { formatJSONResponse } from '@libs/api-gateway'
+import replyFlexMessage from './reply-flex-message.json'
 
 const channelAccessToken = process.env.ACCESS_TOKEN!
 const channelSecret = process.env.CHANNEL_SECRET!
@@ -13,14 +14,25 @@ const config: Line.ClientConfig = {
 }
 const client = new Line.Client(config)
 
-const eventHandler = async (event: Types.MessageEvent): Promise<any> => {
-  if (event.type !== 'message' || event.message.type !== 'text' || !event.source.userId) {
-    return null
+const eventHandler = async (event: Types.PostbackEvent | Types.MessageEvent): Promise<any> => {
+  if (event.type === 'message' && event.message.type === 'text') {
+    if (event.message.text === 'おすすめ') {
+      return client.replyMessage(event.replyToken, {
+        type: 'flex',
+        altText: 'reply flex yes or no',
+        contents: replyFlexMessage as Line.FlexContainer,
+      })
+    }
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text,
+    })
+  } else if (event.type === 'postback') {
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.postback.data,
+    })
   }
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  })
 }
 
 const handler: Lambda.APIGatewayProxyHandler = async (proxyEevent: Lambda.APIGatewayEvent, _context) => {
