e23c88c518946e37b8900ef096d9c8a45da99384
rnk0085 <rinawkmbi4k8@gmail.com>
2023-02-26 09:47:54 +0900

    fix: 修正

---
+++ src/functions/bot/last-flex-message.ts
@@ -0,0 +1,55 @@
+import * as Line from '@line/bot-sdk'
+
+// FIXME
+export const last = (): Line.FlexContainer => {
+  return {
+    "type": "bubble",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "THANK YOU",
+          "color": "#06C755",
+          "align": "start",
+          "weight": "bold"
+        }
+      ]
+    },
+    "hero": {
+      "type": "image",
+      "url": "https://www.city.fukuyama.hiroshima.jp/uploaded/image/11535.jpg",
+      "size": "5xl",
+      "margin": "none"
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "ご利用ありがとうございました",
+          "wrap": true
+        },
+        {
+          "type": "text",
+          "text": "また使ってね！"
+        },
+        {
+          "type": "separator",
+          "margin": "md"
+        },
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "お店の詳細へ",
+            "uri": "http://linecorp.com/"
+          },
+          "style": "primary"
+        }
+      ]
+    }
+  }
+}
